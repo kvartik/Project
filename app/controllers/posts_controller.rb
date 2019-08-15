94ecd242687f68cf511c1edb161ead5aaf59a304
kvartik <kvartik0606@gmail.com>
2019-08-15 18:51:53 +0300

    new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,39 @@
 class PostsController < ApplicationController
   def index
-@post = Post.all
+    @post = Post.all
   end
 
   def new
-@post = Post.new
+    @post = Post.new
   end
 def show
-@post = Post.find(params[:id])
+  @post = Post.find(params[:id])
 end
   def create
 #render plain: params[:post].inspect
-@post = Post.new(post_params)
-
-
-if(@post.save)
-redirect_to @post
-else
-  render 'new'
+    @post = Post.new(post_params)
+    if(@post.save)
+    redirect_to @post
+    else
+      render 'new'
   end
 
 end
-  private def post_params
-params.require(:post).permit(:title, :body)
+
+    private def post_params
+    params.require(:post).permit(:title, :body)
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if(@post.update(post_params))
+    redirect_to @post
+    else
+      render 'edit'
+  end
+end
 end
